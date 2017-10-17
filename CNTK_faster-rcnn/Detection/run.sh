14d7514cad32d6fa6bbcec5d0e0deadd857a6e16
Rita Zhang <rita.z.zhang@gmail.com>
2017-10-16 21:11:36 -0700

    Update web server to sanic

--- CNTK_faster-rcnn/Detection/run.sh
+++ CNTK_faster-rcnn/Detection/run.sh
@@ -2,4 +2,5 @@
 source /root/anaconda3/bin/activate root
 pip install easydict
 pip install azure-ml-api-sdk==0.1.0a9
-python /app/app.py
+pip install sanic
+python3 /app/app.py
--- CNTK_faster-rcnn/Detection/server.py
+++ CNTK_faster-rcnn/Detection/server.py
@@ -1,6 +1,5 @@
 import json
 from predict import get_result, get_model
-from flask import request, Response
 
 class Server:
   model = None
@@ -11,12 +10,10 @@ class Server:
   def server_running(self):
     return 'Server is running...'
 
-  def predict(self):
-    incoming = request.get_json()
+  def predict(self, request):
+    incoming = request.json
     filename  = incoming['filename']
     print (filename)
     prediction = get_result(filename, self.model)
-    print (prediction)  
-    resp = Response(prediction)
-    return resp
-    
+    print (prediction)
+    return prediction
